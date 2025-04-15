da20c1b9dc3f0eead118bafc4a3f6bbab609c70b
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-04-14 22:43:09 -0300

    1.2 Procesamiento de excepciones

--- Python/Tecnicatura3Py/Excepciones/Leccion01/menejo_excepciones.py
+++ Python/Tecnicatura3Py/Excepciones/Leccion01/menejo_excepciones.py
@@ -1,4 +1,10 @@
+resultado = None
+a = 10
+b = 0
 try:
-    10/0
+    resultado = a / b # modificamos
 except Exception as e:
     print(f'Ocurrio un error: {e}')
+
+    print(f'El resultado es: {resultado}')
+    print('seguimos ...')
