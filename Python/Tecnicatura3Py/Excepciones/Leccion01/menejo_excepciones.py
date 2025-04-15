e170624b0559f0a424fb9395b4d7e6ab567baf82
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-04-14 23:50:26 -0300

    1.5 Bloques else y finally al manejar excepciones

--- Python/Tecnicatura3Py/Excepciones/Leccion01/menejo_excepciones.py
+++ Python/Tecnicatura3Py/Excepciones/Leccion01/menejo_excepciones.py
@@ -11,6 +11,9 @@ except ZeroDivisionError as e:
     print(f'ZeroDivicionError - Ocurrio un error: {type(e)}')
 except Exception as e:
     print(f'Exception - Ocurrio un error: {type(e)}')
-
+else:
+    print("No se arrojo ninguna excepcion")
+finally: # Siempre se va a ejecutar
+    print("Ejecucion de este bloque finally")
 print(f'El resultado es: {resultado}')
 print('seguimos ...')
