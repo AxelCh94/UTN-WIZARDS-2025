58cd8038e0c90b440b1a1dd90001422650201a14
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-04-19 22:57:26 -0300

    1.2 Especificar el juego de caracteres de un archivo de texto

--- Python/Tecnicatura3Py/Archivos/Leccion02/manejo_archivos.py
+++ Python/Tecnicatura3Py/Archivos/Leccion02/manejo_archivos.py
@@ -1,9 +1,12 @@
 # Declaramos una variable
 try:
-    archivo = open('prueba.txt', 'w') # La w es de write que sisgnifica escribir
+    archivo = open('prueba.txt', 'w', encoding='utf-8') # La w es de write que sisgnifica escribir
     archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n') # \n es para salto de linea
+    archivo.write('Con esto terminamos')
+    archivo.write('como por ejemplo: acción, ejecucíon y producción\n')
     archivo.write('Con esto terminamos')
 except Exception as e:
     print(e)
 finally: # siempre se ejecuta
     archivo.close() # Con esto se debe cerrar el archivo
+# archivo.write('Todo quedo perfecto')  // este es un error porque el archivo esta cerrado
