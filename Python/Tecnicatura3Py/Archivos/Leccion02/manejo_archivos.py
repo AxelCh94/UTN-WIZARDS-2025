4c9db835107436deebdc012da0b987dccb8162bd
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-04-20 23:22:13 -0300

    1.4 Más formas de trabajar con archivos

--- Python/Tecnicatura3Py/Archivos/Leccion02/manejo_archivos.py
+++ Python/Tecnicatura3Py/Archivos/Leccion02/manejo_archivos.py
@@ -6,7 +6,9 @@ try:
     archivo.write('como por ejemplo: acción, ejecucíon y producción\n')
     archivo.write('las letras son:\n r= read leer, \n a= append anexa,\n w= write escribe, \n x= crea un archivo')
     archivo.write('\n t= esto es para texto, \n b= tipo archivos binario, \n w+= leer y escribe, \n r+= es igual al w+ \n')
+    archivo.write('Saludis a todos los alumnos de la tercnicatura\n')
     archivo.write('Con esto terminamos')
+
 except Exception as e:
     print(e)
 finally: # siempre se ejecuta
