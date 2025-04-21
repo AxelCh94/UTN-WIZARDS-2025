dbb429f3caf9413152efbc2f0fcaea3597b0fb98
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-04-20 22:29:29 -0300

    1.3 Lectura de archivos

--- Python/Tecnicatura3Py/Archivos/Leccion02/manejo_archivos.py
+++ Python/Tecnicatura3Py/Archivos/Leccion02/manejo_archivos.py
@@ -4,6 +4,8 @@ try:
     archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n') # \n es para salto de linea
     archivo.write('Con esto terminamos')
     archivo.write('como por ejemplo: acción, ejecucíon y producción\n')
+    archivo.write('las letras son:\n r= read leer, \n a= append anexa,\n w= write escribe, \n x= crea un archivo')
+    archivo.write('\n t= esto es para texto, \n b= tipo archivos binario, \n w+= leer y escribe, \n r+= es igual al w+ \n')
     archivo.write('Con esto terminamos')
 except Exception as e:
     print(e)
