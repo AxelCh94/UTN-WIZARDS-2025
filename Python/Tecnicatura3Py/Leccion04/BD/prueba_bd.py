8a9be1255a18561e8e5fd51f1cfefc31ebfa4866
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-05-05 22:47:09 -0300

    Clase 4 Postgresql y Python

---
+++ Python/Tecnicatura3Py/Leccion04/BD/prueba_bd.py
@@ -0,0 +1,18 @@
+import psycopg2 # Esto es para poder conecctarnos a Postgre
+
+conexion = psycopg2.connect(
+    user='postgres',
+    password='admin',
+    host='127.0.0.1',
+    port='5432',
+    database= 'test_bd'
+)
+
+cursor = conexion.cursor()
+sentencia = 'SELECT * FROM persona'
+cursor.execute(sentencia) # de esta manera ejecutamos la sentencia
+registro = cursor.fetchall() # Recuperamos todos los registros que seran una lista
+print(registro)
+
+cursor.close()
+conexion.close()
