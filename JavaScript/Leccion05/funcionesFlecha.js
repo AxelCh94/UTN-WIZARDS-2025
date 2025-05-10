2b0ce16dc88578b4ac104bc269a07937cb3ee0fb
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-05-09 21:55:21 -0300

    5.1 Comenzamos con los ejemplos clásicos

---
+++ JavaScript/Leccion05/funcionesFlecha.js
@@ -0,0 +1,20 @@
+
+
+function miFuncion(){
+    console.log('Saludos desde mi funcion')
+}
+
+miFuncion();
+
+let myFuncion = function () {
+console.log('Saludos desde funcion anonima')
+}
+
+// Ahora vamos a crear una funcion flecha
+
+let miFuncionFlecha = () => {
+    console.log('Saludos desde mi funcion flecha');
+}
+
+// Hay mas variantes para funciones flecha que vamos a ir viendo
+miFuncionFlecha();  
